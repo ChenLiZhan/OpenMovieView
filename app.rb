77b8bbaec0d94ba3816bd6696ca8a1d83e680bcf
davidyang <pingjing.yang@gmail.com>
2014-12-07 14:25:55 +0800

    remove unnecessary

--- app.rb
+++ app.rb
@@ -22,11 +22,14 @@ class MovieCrawlerApp < Sinatra::Base
   SimpleNavigation.config_file_paths << File.expand_path('../config', __FILE__)
   # register Sinatra::Namespace
 
+ # set the sinatra configure
   configure :production, :development do
     enable :logging
   end
 
   API_BASE_URI = 'http://localhost:4567'
+  API_VER = '/api/v2/'
+
 
   # helpers do
   #   # RANK_LIST = { '1' => 'U.S.', '2' => 'Taiwan', '3' => 'DVD' }
